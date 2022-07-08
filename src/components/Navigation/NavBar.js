51442314f14c26a7f6fe975e5960a81fde499b16
Christian A <christianaguilar27.07@gmail.com>
2022-07-07 23:05:47 -0500

    banner styles added

--- src/components/Navigation/NavBar.js
+++ src/components/Navigation/NavBar.js
@@ -2,21 +2,28 @@ import React from 'react'
 
 export const NavBar = () => {
   return (
-    <header>
-      <section>
-        <h1><a href="/">AIRLINE</a></h1>
-      </section>
-      <section>
-        <div>
-          en
-        </div>
-        <div>
-          <a href='/'>Home</a>
-          <a href='/'>About</a>
-          <a href='/'>Destination</a>
-          <a href='/'>Contact</a>
-        </div>
-      </section>
+    <header className='header'>
+      <div className='navbar'>
+        <section className='navbar__brand'>
+          <h1><a href="/">AIRLINE</a></h1>
+        </section>
+        <section className='navbar__items'>
+          <div>
+            <select name='languages' defaulValue={'en'} className=''>
+              <option value={'en'}>en</option>
+              <option value={'ru'}>RU</option>
+              <option value={'de'}>DE</option>
+              <option value={'fr'}>FR</option>
+            </select>
+          </div>
+          <div>
+            <a href='/' className='navbar__items--link-active'>Home</a>
+            <a href='/'>About</a>
+            <a href='/'>Destination</a>
+            <a href='/'>Contact</a>
+          </div>
+        </section>
+      </div>
     </header>
   )
 }
--- src/sections/Banner.js
+++ src/sections/Banner.js
@@ -3,12 +3,14 @@ import { ButtonBanner } from '../components/Buttons/ButtonBanner'
 
 export const Banner = () => {
   return (
-    <section>
-      <h2>Visit Canada with prices from $800</h2>
-      <p>Prepare your bags and forget your worries to start enjoying your holidays in this fantastic country.</p>
-      <ButtonBanner
-        text={'BUY NOW'}
-      />
+    <section className='banner'>
+      <div className='banner__info'>
+        <h2 className='banner__title'>Visit Canada with prices from $800</h2>
+        <p className='banner__body'>Prepare your bags and forget your worries <br/> to start enjoying your holidays in this fantastic country.</p>
+        <ButtonBanner
+          text={'BUY NOW'}
+        />
+      </div>
     </section>
   )
 }
